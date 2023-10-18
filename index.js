d0f834584484968f45c59f3514b61822fdb09720
ArneSamson <arnesamson007@gmail.com>
2023-10-18 12:55:44 +0200

    feat: add get request with id and post request with variable name

--- index.js
+++ index.js
@@ -5,15 +5,57 @@ const port = 3000;
 const cors = require('cors');
 app.use(cors());
 
+app.get('/', (req, res) => {
+    res.send('Welcome to my API!');
+});
+
 app.get('/api/v1/messages', (req, res) => {
     res.json(
         {
             status: "success",
             message: "getting messages",
-            data: []
+            data: {
+                messages: [
+                    {
+                        user: "Norm Scully",
+                        message: "sandwiches are good"
+                    },
+                    {
+                        user: "Hitchcock",
+                        message: "Beaver trap"
+                    }
+                ]
+            }
         }
     );
-    res.send('hiiiiii 🎃')
+});
+
+app.get('/api/v1/messages/:id', (req, res) => {
+    const id = req.params.id;
+
+    res.json(
+        {
+            status: "success",
+            message: "getting message for id " + id,
+            data: {
+                message: {
+                    user: "Norm Scully",
+                    message: "sandwiches are good"
+                }
+            }
+        }
+    );
+});
+
+app.post('/api/v1/messages/:username', (req, res) => {
+    const username = req.params.username;
+
+    res.json(
+        {
+            status: "success",
+            message: "posting a new message for user " + username,
+        }
+    );
 });
 
 app.listen(port, () => {
