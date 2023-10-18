69cb6e1df94e06b8603252651f385a9c7152c4df
ArneSamson <arnesamson007@gmail.com>
2023-10-18 20:26:25 +0200

    feat: connect to messages collection in database

--- models/messageModel.js
+++ models/messageModel.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
 
-const messageSchema = new mongoose.Schema({
+const messageSchema = new mongoose.Schema(
+  {
     user: String,
     text: String,
-});
+  },
+  {
+    collection: 'messages',
+  }
+);
 
 module.exports = mongoose.model('Message', messageSchema);
