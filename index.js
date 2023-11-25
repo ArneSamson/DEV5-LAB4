e2df935a2792db0e3e3bfd084d434b6fcb5614e3
ArneSamson <arnesamson007@gmail.com>
2023-11-25 12:22:44 +0100

    feat: add score route to API index

--- index.js
+++ index.js
@@ -15,9 +15,14 @@ const messagesRouter = require('./router/api/v1/message.js');
 
 app.use('/api/v1/messages', messagesRouter);
 
+const scoreRouter = require('./router/api/v1/score.js');
+
+app.use('/api/v1/score', scoreRouter);
+
 mongoose.connect(process.env.MONGODB, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
+    useUnifiedTopology: true,   
+    // dbName: 'message',
 });
 
 app.listen(port, () => {
